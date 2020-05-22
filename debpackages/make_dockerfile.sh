c085d4a9e935d50e6c8d8c62e048805be98f8d59
ogak <ogak@home.pgr>
2020-05-22 16:42:24 +0200

    automation top docker stuff

--- debpackages/make_dockerfile.sh
+++ debpackages/make_dockerfile.sh
@@ -3,10 +3,7 @@ dockerfile="${1:-Dockerfile}"
 source .env
 
 echo """FROM $DISTRO:$DISTRO_VERSION
-#COPY 19-sources.list /etc/apt/sources.list
 WORKDIR /flobby
 ADD flobby_installer.sh flobby_installer.sh
 RUN chmod +x /flobby/flobby_installer.sh
-#ENTRYPOINT ["ls", "flobby_installer.sh"]
-#ENTRYPOINT [ls]
 """ > Dockerfile
--- debpackages/make_flobby.sh
+++ debpackages/make_flobby.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-#read -p "Did you checkup .env?  " -n 1 -r
-#if [[ ! $REPLY =~ ^[Yy]$ ]]
-#then
-#  exit 1
-#fi
+read -p "Did you checkup .env?  " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
 
 printf "\ncreating dockerfile"
 ./make_dockerfile.sh
